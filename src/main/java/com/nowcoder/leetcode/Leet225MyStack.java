5eb4c4124e83bcd20805ed27afc24cf1aae03728
fanjingyuan <1024042400@qq.com>
2018-04-18 18:53:30 +0800

    little

--- src/main/java/com/nowcoder/leetcode/Leet225MyStack.java
+++ src/main/java/com/nowcoder/leetcode/Leet225MyStack.java
@@ -3,6 +3,12 @@ package com.nowcoder.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 使用队列实现栈
+ *
+ * @link https://leetcode-cn.com/problems/implement-stack-using-queues/description/
+ * 将队列看成一个数组.
+ **/
 public class Leet225MyStack {
     private int index = -1;
     private List<Integer> list = new ArrayList<>();
---
+++ src/main/java/com/nowcoder/leetcode/Leet232MyQueue.java
@@ -0,0 +1,56 @@
+package com.nowcoder.leetcode;
+
+import java.util.Stack;
+
+/**
+ * 用栈实现队列
+ *
+ * @link https://leetcode-cn.com/problems/implement-queue-using-stacks/description/
+ *
+ * 用俩个栈来实现.
+ */
+public class Leet232MyQueue {
+
+    private Stack<Integer> s1 = new Stack<>();
+    private Stack<Integer> s2 = new Stack<>();
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(!s2.empty()) {
+            return s2.pop();
+        } else {
+            putS1ToS2();
+            if(!s2.empty())
+                return s2.pop();
+        }
+        return 0;
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(!s2.empty()) {
+            return s2.peek();
+        } else {
+            putS1ToS2();
+            if(!s2.empty())
+                return s2.peek();
+        }
+        return 0;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.empty() && s2.empty();
+    }
+
+    private void putS1ToS2() {
+        while(!s1.empty()) {
+            s2.push(s1.pop());
+        }
+    }
+}
